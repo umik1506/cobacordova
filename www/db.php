a1c0982790029d3acd903aa81514997acfdb0d34
umik1506 <80735754+umik1506@users.noreply.github.com>
2021-03-26 18:10:06 +0700

    upload tugas

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("sql204.epizy.com","epiz_28182655","aIlcORHm4If","epiz_28182655_db_phonegap") or die ("could not connect database");
+?>
